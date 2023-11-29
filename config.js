c241e0669c9fcc253f15715d836fd5619288ebe6
dathuynh05 <152080627+dathuynh05@users.noreply.github.com>
2023-11-29 19:56:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: "bimatt",
+    introTitle: 'Hé luuuuu',
+    introDesc: `Gặp cậu tỏ tình yêu thương, tui ước được cậu thưởng tình yêu tooo :3`,
+    btnIntro: 'Hihihi:33',
+    title: 'Tui có điều này muốn hỏi nhưng mà cậu nhớ phải trả lời thật lòng đó nhaaa.',
+    desc: 'Làm người yêu tui nhé?? ><',
+    btnYes: 'Đồng ý nè <33',
+    btnNo: 'Không nhaa :3',
+    question: 'Sáng tỏ lời yêu rồi nè vậy từ giờ tới chiều lòng cậu được khongg :3',
+    btnReply: 'Nhắn gửi yêu thươngg <3',
+    reply: 'Quá là được lunn <33333333',
+    messDesc: 'Từ giờ để tui chở cậu đi học nhéee :33',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100046662342204' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
